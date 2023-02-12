b32eb4d6ec7b86d3e3023abe633bccfb2fd9e83a
avearmin <117543612+avearmin@users.noreply.github.com>
2023-02-11 22:53:12 -0500

    Add computer move logic in rock-paper-scissors game
    The function, `getComputerChoice()` uses the `getRandomInt` function to assign a number from 0 - 2 to the variable `computerChoice`. The function then takes the number and converts it to "rock", "paper" or "scissors" to return back to the program. This ensures the computer can generate its move in the rock-paper-scissors game.

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
 }
 
+function getComputerChoice() {
+    let computerChoice = getRandomInt(2);
+    return computerChoice === 0 ? "rock"
+    : computerChoice === 1 ? "paper"
+    : computerChoice === 2 ? "scissors"
+    : "something unexpected happened";
+}
